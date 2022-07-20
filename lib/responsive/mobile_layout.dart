9fe320a3634a2ca7c656bc6106a2a95b150e6e86
Ahmad <muahmad710@gmail.com>
2022-07-20 18:34:42 +0500

    Bottom navigation UI

--- lib/responsive/mobile_layout.dart
+++ lib/responsive/mobile_layout.dart
@@ -1,17 +1,63 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social/Models/model_user.dart';
 import 'package:flutter_social/Providers/user_provider.dart';
+import 'package:flutter_social/utils/colors.dart';
 import 'package:provider/provider.dart';
 
-class MobileLayout extends StatelessWidget {
+class MobileLayout extends StatefulWidget {
   const MobileLayout({super.key});
+
+  @override
+  State<MobileLayout> createState() => _MobileLayoutState();
+}
+
+class _MobileLayoutState extends State<MobileLayout> {
+
+  int _page = 0;
 
   @override
   Widget build(BuildContext context) {
     ModelUser? user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       body: Center(
-        child: user != null? Text(user.username) : const CircularProgressIndicator(),
+        child: Text("this is mobile"),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: _page == 0? Colors.white : secondaryColor,),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search , color: _page == 1? Colors.white : secondaryColor,),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle, color: _page == 2? Colors.white : secondaryColor,),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color: _page == 3? Colors.white : secondaryColor,),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: _page == 4? Colors.white : secondaryColor,),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+        ],
+        onTap: (value) {
+          setState(() {
+            _page = value;
+          });
+        },
       ),
     );
   }
--- lib/responsive/web_layout.dart
+++ lib/responsive/web_layout.dart
@@ -1,17 +1,56 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social/Models/model_user.dart';
 import 'package:flutter_social/Providers/user_provider.dart';
+import 'package:flutter_social/utils/colors.dart';
 import 'package:provider/provider.dart';
 
-class WebLayout extends StatelessWidget {
+class WebLayout extends StatefulWidget {
   const WebLayout({super.key});
 
+  @override
+  State<WebLayout> createState() => _WebLayoutState();
+}
+
+class _WebLayoutState extends State<WebLayout> {
   @override
   Widget build(BuildContext context) {
     ModelUser? user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       body: Center(
-        child: user != null? Text(user.username) : const CircularProgressIndicator(),
+        child: Text("this is web")
+      ),
+       bottomNavigationBar: CupertinoTabBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "",
+            backgroundColor: primaryColor,   
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "",
+            backgroundColor: primaryColor
+          ),
+        ],
       ),
     );
   }
