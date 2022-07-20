d01dafd8e88fce1aa6f0c807e8e64cbc437f3886
Ahmad <muahmad710@gmail.com>
2022-07-20 05:18:57 +0500

    Added Model User class

---
+++ lib/Models/model_user.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ModelUser {
+  final String username;
+  final String bio;
+  final String uid;
+  final String email;
+  final List followers;
+  final List following;
+  final String photoUrl;
+
+  ModelUser({
+    required this.username,
+    required this.bio,
+    required this.uid,
+    required this.email,
+    required this.followers,
+    required this.following,
+    required this.photoUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'username': username,
+        'bio': bio,
+        'uid': uid,
+        'email': email,
+        'followers': followers,
+        'following': following,
+        'photoUrl': photoUrl,
+      };
+}
