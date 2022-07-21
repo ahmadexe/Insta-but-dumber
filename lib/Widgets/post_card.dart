cb99190a079e33db94eb4a4c0c8ec28ca139e825
Ahmad <muahmad710@gmail.com>
2022-07-22 02:00:24 +0500

    Post card UI Completed

--- lib/Widgets/post_card.dart
+++ lib/Widgets/post_card.dart
@@ -21,10 +21,13 @@ class PostCard extends StatelessWidget {
                   children: [
                     CircleAvatar(
                       radius: 20,
-                      backgroundImage: NetworkImage('https://images.unsplash.com/photo-1658087318893-59f40d1af71f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'),
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1658087318893-59f40d1af71f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'),
                     ),
                     const SizedBox(width: 8),
-                    Text('username', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text('username',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
                   ],
                 ),
                 IconButton(
@@ -36,12 +39,12 @@ class PostCard extends StatelessWidget {
             //! Image portion
           ),
           SizedBox(
-              height: MediaQuery.of(context).size.height * 0.35,
-              width: double.infinity,
-              child: Image.network(
-                'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                fit: BoxFit.cover,
-              ),
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              fit: BoxFit.cover,
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -63,16 +66,62 @@ class PostCard extends StatelessWidget {
                 icon: const Icon(Icons.send),
                 onPressed: () {},
               ),
-              Expanded(child: Align(
-                alignment: Alignment.bottomRight,
-                child: IconButton(
-                  icon: const Icon(Icons.bookmark_border),
-                  onPressed: () {},
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.bookmark_border),
+                    onPressed: () {},
+                  ),
                 ),
-              ),)
+              )
             ],
           ),
-        ],   
+          const Divider(),
+          //! Comments and Likes portion
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2!
+                        .copyWith(fontWeight: FontWeight.w800),
+                    child: Text("1,234 Likes",
+                        style: Theme.of(context).textTheme.bodyText2)),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 8),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'username',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                          text: ' This is a caption',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      "View all 150 comments...",
+                      style: TextStyle(color: secondaryColor, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
