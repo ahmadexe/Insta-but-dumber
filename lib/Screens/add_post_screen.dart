fcf6f36071402e0c3498bf224110ca5d47e03282
Ahmad <muahmad710@gmail.com>
2022-07-21 09:36:27 +0500

    Add post ui app bar added

--- lib/Screens/add_post_screen.dart
+++ lib/Screens/add_post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social/utils/colors.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({super.key});
@@ -10,8 +11,25 @@ class AddPost extends StatefulWidget {
 class _AddPostState extends State<AddPost> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(onPressed: (){}, icon: Icon(Icons.upload))
+    // return Center(
+    //   child: IconButton(onPressed: (){}, icon: Icon(Icons.upload))
+    // );
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
+          title: Text("Add Post"),
+          actions: [
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Post",
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ))
+          ]),
     );
   }
 }
