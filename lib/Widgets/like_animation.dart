ccaa67dde7325eb16f8180b4337fb335f546d46f
Ahmad <muahmad710@gmail.com>
2022-07-24 00:34:05 +0500

    Creating like animation

--- lib/Widgets/like_animation.dart
+++ lib/Widgets/like_animation.dart
@@ -5,16 +5,48 @@ class LikeAnimation extends StatefulWidget {
   final bool isAnimating;
   final Duration duration;
   final VoidCallback? onEnd;
-  final bool isLiked; 
-  const LikeAnimation({super.key, required this.child, this.isAnimating = false, this.duration = const Duration(milliseconds: 500), this.onEnd, this.isLiked = false});
+  final bool isLiked;
+  const LikeAnimation(
+      {Key? key,
+      required this.child,
+      this.isAnimating = false,
+      this.duration = const Duration(milliseconds: 500),
+      this.onEnd,
+      this.isLiked = false})
+      : super(key: key);
 
   @override
   State<LikeAnimation> createState() => _LikeAnimationState();
 }
 
-class _LikeAnimationState extends State<LikeAnimation> with SingleTickerProviderStateMixin {
+class _LikeAnimationState extends State<LikeAnimation>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  
+  late Animation<double> scale;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 2));
+    scale = Tween<double>(begin: 1, end: 1.2).animate(_animationController);
+  }
+
+  @override
+  void didUpdateWidget(covariant LikeAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isAnimating != oldWidget.isAnimating) {
+      startAnimation();
+    } 
+  }
+
+  void startAnimation() async {
+    if (widget.isAnimating || widget.isLiked) {
+      await _animationController.forward();
+      await _animationController.reverse();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
