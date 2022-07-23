3bbbb254dbf25c69c3d0c72e52db5ec7db633fb8
Ahmad <muahmad710@gmail.com>
2022-07-23 23:44:11 +0500

    Creating like animation

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -16,26 +16,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: SvgPicture.asset('assets/ic_instagram.svg', color: primaryColor, height: 30,),
-        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.send))],
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 30,
+        ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.send))],
         backgroundColor: mobileBackgroundColor,
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              return PostCard(
-                snap: snapshot.data!.docs[index],
-              );
-            },
-          );
-        }
-      ),
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                return PostCard(
+                  snap: snapshot.data!.docs[index],
+                );
+              },
+            );
+          }),
     );
   }
 }
