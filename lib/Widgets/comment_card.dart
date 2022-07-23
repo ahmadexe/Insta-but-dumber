829560aa539f33866c6c90eac21c4df0205419ad
Ahmad <muahmad710@gmail.com>
2022-07-24 04:01:28 +0500

    Comment card UI now working

--- lib/Widgets/comment_card.dart
+++ lib/Widgets/comment_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social/utils/colors.dart';
 
 class CommentCard extends StatefulWidget {
   const CommentCard({super.key});
@@ -10,6 +11,49 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundImage: NetworkImage(
+              "https://i.pravatar.cc/300",
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16,right: 8),
+                  child: RichText(text: TextSpan(
+                    children: [
+                      TextSpan(
+                      text: "John Doe",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "   Lorem ipsum dolor sit amet.",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                    ),
+                    ]
+                    )
+                  ),
+                ),
+              ],
+            ),
+          )      
+        ],
+      ),  
+    );
   }
 }
