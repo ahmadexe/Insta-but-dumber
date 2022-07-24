5d592dbe1c9720e0da9de4366451b8c3b017b0ad
Ahmad <muahmad710@gmail.com>
2022-07-25 01:06:31 +0500

    Profile UI now working

--- lib/Screens/user_profile_screen.dart
+++ lib/Screens/user_profile_screen.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_social/Models/model_user.dart';
+import 'package:flutter_social/Providers/user_provider.dart';
 import 'package:flutter_social/Screens/home_screen.dart';
 import 'package:flutter_social/responsive/mobile_layout.dart';
 import 'package:flutter_social/utils/colors.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({super.key});
@@ -14,6 +18,7 @@ class UserProfile extends StatefulWidget {
 class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
+    ModelUser? user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       appBar: AppBar(
         title: const Text("User Profile"),
@@ -45,16 +50,90 @@ class _UserProfileState extends State<UserProfile> {
             const Text("Username"),
             const Text("This is a bio"),
             const SizedBox(height: 10),
-            Container(
-              alignment: Alignment.center,
-              height: 30,
-              width: MediaQuery.of(context).size.width * 0.8,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Colors.grey[900],
-              ),
-              child: Text("Edit Profile", style: TextStyle(fontSize: 16),),
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 30,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Colors.grey[900],
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 2.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(2.0, 2.0), // shadow direction: bottom right
+                        )
+                      ],
+                    ),
+                    child: Text(
+                      "Edit Profile",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 5),
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    height: 30,
+                    width: 30,
+                    child: Icon(Icons.person_add),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.grey[900],
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 2.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(2.0, 2.0), // shadow direction: bottom right
+                          )
+                        ],
+                      ),
+                  ),
+                )
+              ],
             ),
+            const SizedBox(height: 40),
+            FutureBuilder(
+                future: FirebaseFirestore.instance.collection('posts').get(),
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return Expanded(
+                    child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisSpacing: 1,
+                          mainAxisSpacing: 1,
+                          crossAxisCount: 3,
+                        ),
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          return snapshot.data!.docs[index].data()['uid'] == user!.uid?
+                           Container(
+                            height: MediaQuery.of(context).size.height / 5,
+                            width: MediaQuery.of(context).size.width / 3.3,
+                            child: Card(
+                              child: Image.network(
+                                snapshot.data!.docs[index].data()['postUrl'],
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          )
+                          :
+                          Container(
+                          );
+                        }),
+                  );
+                }))
           ],
         ),
       ),
